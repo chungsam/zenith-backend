ca01f16c0a75998606c9ab60a96812807d133edc
Sam Chung <chungsam@outlook.com>
2018-02-22 09:41:08 -0800

    Add ActivityType seed data on start

--- routes/api/activityTypes.js
+++ routes/api/activityTypes.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-var Event = require('../../models/activityType');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
 
+var ActivityType = require('../../models/activityType');
+
+
+/* add seed data to db */
+// first remove existing data
+ActivityType.collection.drop();
+
+// add seed data
+var activityTypeSeedData = require('../../data/activityTypeSeed').activityTypes;
+ActivityType.insertMany(activityTypeSeedData);
+
+/* routes */
 router.get('/', (req, res) => {
     res.send('hi from activityTypes!');
 });
