623a9a29279b9d21e000c0a969d05e9386bda41a
Sam Chung <chungsam@outlook.com>
2018-02-22 11:06:57 -0800

    Change mLab db

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
     databaseLocal: 'mongodb://localhost/zenithDB',
-    databaseMLab: 'mongodb://admin:admin@ds243768.mlab.com:43768/comp3913zenithdb'
+    databaseMLab: 'mongodb://admin:admin@ds143778.mlab.com:43778/comp3913zenithdb'
 }
