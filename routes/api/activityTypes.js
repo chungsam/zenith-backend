3e5d3ef833f9d543b9401412f844ec2b4f9de726
Sam Chung <chungsam@outlook.com>
2018-02-22 11:42:10 -0800

    Add CRUD routes for activity types api

--- routes/api/activityTypes.js
+++ routes/api/activityTypes.js
@@ -19,8 +19,86 @@ ActivityType.collection.count({}, (err, res) => {
 })
 
 /* routes */
-router.get('/', (req, res) => {
 
+// get all activity types
+router.get('/', (req, res) => {
+    ActivityType.find({}, (err, data) => {
+        if (err) res.send(err);
+        res.json(data);
+    })
 });
+
+// get activity type by id
+router.get('/:id', (req, res) => {
+    ActivityType.findById(req.params.id, (err, activityType) => {
+        if (err) res.send(err);
+        res.json(activityType);
+    })
+});
+
+// add activity type
+router.post('/', (req, res) => {
+    var newActivityType = new ActivityType();
+
+    if (!req.body.desc) {
+        res.status(400);
+        res.json({
+            "error": "Bad data, could not add to database."
+        })
+    } else {
+        newActivityType.desc = req.body.desc;
+
+        // finally, save to db
+        newActivityType.save((err, data, numAffected) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json(data);
+
+                console.log(`Added new activity type: ${newActivityType.desc}`);
+            }
+        })
+    }
+});
+
+// update activity type
+router.put('/:id', (req, res) => {
+    if (!req.body.desc) {
+        res.status(400);
+        res.send({
+            "error": "Bad data, could not update."
+        });
+    } else {
+        ActivityType.findById(req.params.id, (err, activityType) => {
+            if (err) {
+                res.send(err);
+            } else {
+                activityType.desc = req.body.desc;
+
+                res.json({"message": "Successfully updated activity type."});
+
+                console.log(`Updated activity type: ${activityType.desc}`);
+            }
+        });
+    }
+});
+
+// delete activity type
+router.delete('/:id', (req, res) => {
+    ActivityType.remove({
+        _id: req.params.id
+    }, (err, activityType) => {
+        if (err) {
+            res.send(err);
+        } else {
+
+            res.json({
+                "message": "Successfully deleted activity type."
+            });
+
+            console.log(`Deleted activity type: ${activityType.desc}`);
+        }
+    })
+})
 
 module.exports = router;
