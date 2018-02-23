af67fc0d98b40f1d69a3dfa4acd333ae40d42c7f
Sam Chung <chungsam@outlook.com>
2018-02-23 14:34:40 -0800

    Prepare to deploy to heroku

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/admin', index);
 
 // api routes
 app.use('/api/activityTypes', activityTypesApiRoutes);
